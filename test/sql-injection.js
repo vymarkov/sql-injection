7729446a444f88e88ff05fc1d194d0d25d455dfb
Ghafran Abbas <ghafranabbas@gmail.com>
2014-04-03 19:55:47 +0000

    initial code

---
+++ test/sql-injection.js
@@ -0,0 +1,91 @@
+process.env.NODE_ENV = 'test';
+
+var request = require('supertest');
+var xpect = require('chai').expect;
+var express = require('express');
+var app = express();
+var sqlinjection = require('../lib/sql-injection');
+
+describe('basic tests', function() {
+
+    it('returns function', function(done) {
+
+        xpect(sqlinjection).to.be.a('function');
+        done();
+    });
+});
+
+
+describe('supertests', function() {
+
+    before(function(done) {
+        
+        app.use(sqlinjection);
+        app.get('/test', function(req, res) {
+            res.send(200, {});
+        });
+        app.get('/users/:uid', function(req, res) {
+            res.send(200, {});
+        });
+        app.post('/body', function(req, res) {
+            res.send(200, {});
+        });
+        done();
+    });
+
+    after(function(done) {
+        done();
+    });
+
+    it('can call app', function(done) {
+
+        request(app)
+          .get('/test')
+          .expect(200, done);
+    });
+
+    it('querystring no sql', function(done) {
+
+        request(app)
+          .get('/test')
+          .expect(200, done);
+    });
+
+    it('param no sql', function(done) {
+
+        request(app)
+          .get('/users/1')
+          .expect(200, done);
+    });
+    
+    it('body no sql', function(done) {
+
+        request(app)
+          .post('/body')
+          .send({stuff: 'somestuff'})
+          .expect(200, done);
+    });
+    
+    it('querystring with sql', function(done) {
+
+        request(app)
+          .get('/test?attack=' + escape('select * from table;'))
+          .expect(403, done);
+    });
+
+    it('param with sql', function(done) {
+
+        request(app)
+          .get('/users/' + escape('select * from table;'))
+          .expect(403, done);
+    });
+    
+    it('body with sql', function(done) {
+
+        // request(app)
+        //   .post('/body')
+        //   .send({stuff: 'select * from table;'})
+        //   .expect(403, done);
+        done();
+    });
+});
